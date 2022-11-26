7cdfac20853419d51b6c7d17f8a79b686a5e2aa0
AbhinavArora21 <96251974+AbhinavArora21@users.noreply.github.com>
2022-11-26 18:43:10 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,27 @@
+public class second {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 98;
+        int c = 12;
+        int d = 23;
+
+        int prece, prece1, prece2, prece3;
+
+        prece = (a*b/c)+d;
+
+        prece1 = a*b/2+c-d;
+
+        prece3 = b/a/c/a;
+
+        prece2 = a+b-d%10 *2;
+
+
+        System.out.println(prece3);
+        System.out.println(prece);
+        System.out.println(a+b);
+        System.out.println(prece1);
+        System.out.println(prece2);
+        System.out.println(b/a);
+
+    }
+}
