83eeedd6e863e58e3c1b7c2e2ed3baef262ad63a
AbhinavArora21 <abhinava365366@gmail.com>
2023-10-01 05:55:09 +0530

    java

--- Basic_problems/first.java
+++ Basic_problems/first.java
@@ -1,1 +1,10 @@
-helo kive aa
+import java.util.*;
+
+public class first {
+    public static void main(String[] args) {
+        // Your code starts here
+        System.out.println("Hello, World!");
+
+        // Your code ends here
+    }
+}
---
+++ Java interview questions/problem1.java
@@ -0,0 +1,8 @@
+// package Java interview questions;
+//               Program 1 - What is the output for System.out.println(-6+4*2); ?
+
+public class problem1 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
