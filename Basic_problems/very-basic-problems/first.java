9e6162725b2c7386629a1ae0c9872bc0b52d00b2
AbhinavArora21 <abhinava365366@gmail.com>
2023-12-28 23:30:22 +0530

    updated and question solved

---
+++ Basic_problems/very-basic-problems/first.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class first {
+    public static void main(String[] args) {
+        // Your code starts here
+        System.out.println("Hello, World!");
+
+        // Your code ends here
+    }
+}
